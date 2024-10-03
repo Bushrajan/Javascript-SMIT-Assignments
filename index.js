a7470c6379062fa936268c66d443f4a93184966b
Bushrajan <darkprince67890@gmail.com>
2024-10-03 19:13:48 +0500

    complete props

---
+++ index.js
@@ -0,0 +1,124 @@
+// ==========================================================================
+// ---------------------------------------------------------------
+// let 123variable
+// let first name
+// let 7eleven
+// let your-variable
+// ---------------------------------------------------------------
+// let myVariable = 10;
+// myVariable += 5; //15
+// myVariable -= 3; // 7
+// myVariable *= 2; //20
+// console.log(myVariable); //24
+// ---------------------------------------------------------------
+// let number1 = +prompt("Enter number 1: ");
+// let number2 = +prompt("Enter number 2: ");
+// let result = number1 + number2;
+// console.log(result);
+// ---------------------------------------------------------------
+// let booktitle = "In Search of Lost Time"
+// let bookauthor = "Marcel Proust"
+// let bookyear = "1913"
+// console.log(` The book 'Title' is " ${booktitle}" by Author "${bookauthor}" was published in Year "${bookyear}". `);
+// The book 'Title' is " In Search of Lost Time" by Author "Marcel Proust" was published in Year "1913".
+//------------------------------------------------------------
+// let number = +prompt("Enter Number: ");
+// if (number % 2 === 0) {
+//     console.log("Even number");
+// }
+// else {
+//     console.log("Odd number");
+// }
+// ---------------------------------------------------------
+
+// let score = +prompt("Enter Score less than 100 :")
+
+// if (score > 90 && score <= 100) {
+//     console.log("Grade A");
+// }
+
+// else if (score > 80 && score < 89) {
+//     console.log("Grade B");
+// }
+
+// else if (score > 70 && score < 79) {
+//     console.log("Grade C");
+// }
+
+// else if (score > 60 && score < 69) {
+//     console.log("Grade D");
+// }
+
+// else {
+//     console.log("Grade F");
+// }
+// ---------------------------------------------------------
+
+// let num1 = +prompt("Enter no1: ")
+// let num2 = +prompt("Enter no2: ")
+// let num3 = +prompt("Enter no3: ")
+
+// if (num1 > num2 && num2 < num3) {
+//     console.log("Maximum of three number.");
+// }
+// else {
+//     console.log("Not a Maximum number.");
+// }
+
+// ---------------------------------------------------------
+// let sum_of_all_even_number= 0;
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         sum_of_all_even_number += i;
+//     }
+// }
+// console.log("The sum of all even numbers from 1 to 100 is: " + sum);
+
+// ---------------------------------------------------------
+ 
+//  var a = 5; b = 3;  c = 7; result = a++  -  ++c  +  b    +  ++c  - b++;
+// console.log(result);
+
+//  var a = 1; b = 2;  c = 0; result = ++a  -  --a  +  ++c  +    c  - a++   + --b;
+// console.log(result);
+
+//  var a = 2; b = 4;  c = 6; result = a++  -  --b  +  c--  +  b++  -  ++c;
+// console.log(result);
+
+//  var a = 10; b = 5; c = 8; result = ++a  +  --b  -  c++  -  ++c  +  b--;
+// console.log(result);
+
+//  var a = 3; b = 2;  c = 1; result = b--  -  --a  +  ++c  -  a++  +  ++b  - a;
+// console.log(result);
+
+//  var a = 1; b = 5;  c = 3; result = ++c  +  a--  -  b++  +  c--  -  --a;
+// console.log(result);
+
+//  var a = 7; b = 0;  c = 1; result = ++c  -  a--  +  --b  +  b++  -  --c  + c;
+// console.log(result);
+
+//  var a = 3; b = 4;  c = 9; result = --a  +  ++b  +  c--  -  b++  +  ++c  - --c - --b;
+// console.log(result);
+
+//  var a = 6; b = 1;  c = 3; result = c++  -    b  +  a -- -  --c  +  b; 
+// console.log(result);
+
+// var a = 10; b = 5; c = 8; result = a    +  --b  -   c   -  ++c  +  b--  + a++;
+// console.log(result);
+// ---------------------------------------------------------
+
+// console.log(undefined && false);
+// console.log(true && false);
+// console.log(true && undefined);
+// console.log(undefined && undefined);
+// console.log('undefined' || 'true');
+// console.log(null || 'true');
+// console.log(null || undefined);
+// console.log(null && true || false);
+// console.log(null && true || false && true);
+// console.log(null && true || true && true);
+// console.log((null && true) || (true && 1));
+
+// ---------------------------------------------------------
+
+
